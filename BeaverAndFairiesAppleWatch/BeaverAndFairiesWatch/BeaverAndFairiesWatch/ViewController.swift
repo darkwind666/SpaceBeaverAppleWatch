99725ddbfffcb8b54b345bcf2ecfaaed116a1651
darkwinddev <darkwinddev@gmail.comgit config --global user.email arteomchik@gmail.com>
2017-02-06 22:53:35 +0300

    Create start project

---
+++ BeaverAndFairiesAppleWatch/BeaverAndFairiesWatch/BeaverAndFairiesWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeaverAndFairiesWatch
+//
+//  Created by Sasha Khotiashov on 1/29/17.
+//  Copyright © 2017 Sasha Khotiashov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
